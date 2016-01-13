ccdcde6861a51101d1f6e17a413d2842992e30bf
Anna Lezhikova <anna.v.l@gmail.com>
2016-01-14 12:31:18 +1300

    add posts listing and navigating to post pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var md = require("markdown").markdown;
+var fs = require("fs");
+
+
+var files = fs.readdirSync('./posts')
+
+function parseFile (name) {
+  var filePath = './posts/' + name
+  var file = fs.readFileSync(filePath, "utf-8").split('\n')
+  return {
+    date: name.slice(0,10),
+    title: file[1].slice(8, -1),
+    tags: file[2].slice(6).split(' '),
+    body: md.toHTML(file.slice(5).join('\n')),
+    link: filePath.slice(0, -2) + 'html'
+  }
+}
+
+var posts = files.map(name => parseFile(name))
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {posts: posts})
 });
+
+router.get('/posts/:name', function(req, res, next) {
+  var filename = req.params.name;
+  var postA = posts.filter(file => {return file.link == "./posts/" + filename})
+  res.render('post', {post: postA[0]})
+})
 
 module.exports = router;
